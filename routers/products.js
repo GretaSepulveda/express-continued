3bc38163527ef568ef4fee67ca51d6f8a91d797b
MariaGretaSepulveda <sepulveda.greta@gmail.com>
2019-07-21 17:37:00 -0500

    updates on project

--- routers/products.js
+++ routers/products.js
@@ -3,5 +3,7 @@ const router = express.Router()
 const ProductsController = require('../Controllers/products')
 
 router.get('/products', ProductsController.list)
+router.get('/products/:id', ProductsController.show)
+router.post('/products', ProductsController.create)
 
 module.exports = router
